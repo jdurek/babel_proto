dfa717eda3b41830d9456f06528884ff0cabaffe
Jason <durekjason@gmail.com>
2024-04-08 10:12:12 -0400

    Initial Commit - Minimal crates for the time being

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+
+// Revamping the bevy project from scratch - going to try and focus more on doing subsystems one at a time, and combining them all later.
+
+
+fn main() {
+    println!("Hello, world!");
+}
