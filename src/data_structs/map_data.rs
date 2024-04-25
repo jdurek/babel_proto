1cab3ceb38e42ac78a65e08ae854dbfa7c9733da
Jason Durek <durekjason@gmail.com>
2024-04-25 17:11:58 -0400

    Added dummy 3D instance to start learning 3D camera setup

--- src/data_structs/map_data.rs
+++ src/data_structs/map_data.rs
@@ -30,38 +30,41 @@ pub struct Tile {
 pub struct MapBase {
     pub dim_x: i32,
     pub dim_y: i32,
-    tiles: Vec<Tile>,
-    walls: Vec<Wall>,
-    textures: HashMap<String, String>,
+    pub tiles: Vec<Tile>,
+    pub walls: Vec<Wall>,
+    pub textures: HashMap<String, String>,
     // default_textures: TextureAtlas,
 }
 impl MapBase { 
     // Helper functions to make accessing and handling data easier
+    fn get_tile(&self, x: i32, y:i32) -> Tile {
+        self.tiles[self.get_tile_index(x, y)]
+    }
 
     // Given a X/Y coordinate, update the tile data at said coordinate
-    fn update_tile(&mut self, t: Tile, x: i32, y: i32){
-        let index = self.get_tile_index(x, y);
+    fn update_tile(&mut self, t: Tile, index: usize){
         self.tiles[index as usize] = t;
     }
     // Given a wall index (Use helper function), update the wall value
-    fn update_wall(&mut self, w: Wall, index: i32) {
-        self.walls[index as usize] = w;
+    fn update_wall(&mut self, w: Wall, index: usize) {
+        self.walls[index] = w;
     }
 
-    // Given a coordinate, obtain the tile index
-    fn get_tile_index(&self, x: i32, y: i32) -> i32 {
-        y * self.dim_y + x
+    // Given a coordinate, obtain the tile index (usize)
+    fn get_tile_index(&self, x: i32, y: i32) -> usize {
+        (y * self.dim_y + x) as usize
     }
 
-    // Given a coordinate, obtain the 4 wall indexes
-    fn get_wall_index(&self, x:i32, y:i32) -> [i32; 4] {
+    // Given a coordinate, obtain the 4 wall indexes (usize)
+    fn get_wall_index(&self, x:i32, y:i32) -> [usize; 4] {
         let bottom = y * (self.dim_y + self.dim_x + 1) + x;
         let left = y * (self.dim_y + self.dim_x + 1) + self.dim_x + x;
         let right = left + 1;
         let top = (y + 1) * (self.dim_y + self.dim_x + 1) + x;
 
-        [bottom, left, right, top]
+        [bottom as usize, left as usize, right as usize, top as usize]
     }
+
 }
 
 // Global access to current map - only 1 map is loaded at a time
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,57 @@ pub use data_structs::*;
 
 // Revamping the bevy project from scratch - going to try and focus more on doing subsystems one at a time, and combining them all later.
 
+#[derive(Component)]
+struct CamMarker3D;
 
 // Basic setup - loads in first camera (Which drives the window)
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
-    let mut camera = Camera2dBundle::default();
-    camera.projection.scale = 0.5;
-    camera.transform.translation.x += 1280.0 / 4.0;
-    camera.transform.translation.y += 720.0 / 4.0;
-    
-    commands.spawn(camera);
+fn setup_main_camera(mut commands: Commands){
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(10.0, 12.0, 16.0)
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        CamMarker3D,
+    ));
 
 
 }
 
+fn setup_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+){
+    // circular base
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::rgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+}
+
 
 fn main() {
-    
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, (setup_main_camera, setup_scene))
+        .run();
 }
