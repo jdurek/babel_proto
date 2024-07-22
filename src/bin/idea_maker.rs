8111a6df4d3a6e32083abfb2db944beaf00923ff
Jason Durek <durekjason@gmail.com>
2024-07-22 16:47:56 -0400

    Started adding a few things for 'Princess Maker' menu development

---
+++ src/bin/idea_maker.rs
@@ -0,0 +1,48 @@
+/*
+  idea_maker.rs - Binary file for just testing the "Princess Maker" GUI aspects and making sure stages change correctly
+
+  Goal is for the game state to always be printed to the UI, so that we can see which state we're in, that it changes correctly, etc...
+*/
+
+#![allow(unused)]
+
+use std::thread::spawn;
+
+use bevy::render::{camera, view::RenderLayers};
+
+
+mod prelude {
+    pub use bevy::prelude::*;
+    pub use serde::*;
+    pub use babel_proto::data_structs::map_data::*; 
+    pub use babel_proto::rendering::minimap::*;
+    pub use babel_proto::rendering::first_person::*;
+    pub use babel_proto::rendering::debug_camera::*;
+    pub use babel_proto::states::*;
+}
+
+use prelude::*;
+
+fn main(){
+  // Main app flow - 
+  App::new()
+
+    // Initializes the window of the game (From default or settings file)
+    // Eventual TODO - configure so resolution of window is pulled from config file
+    .add_plugins(DefaultPlugins
+      .set(WindowPlugin{
+          primary_window: Some(Window{ 
+              title: "Ideation".to_string(),
+              resolution: (1024 as f32, 720 as f32).into(),  // TODO - change this later for custom resolution (Or update it on the fly)
+              ..Default::default()
+          }),
+          ..Default::default()
+      }))
+
+    // Trigger loading on global attributes, backend setup
+    // .add_systems()
+
+    // Initialize our global states and sub-systems (Plugins)
+
+    ;
+}
---
+++ src/rendering/maker_sim_menus.rs
@@ -0,0 +1,31 @@
+/* Game Menus 
+    Contains methods, initialization, and layout for the "Princess Maker" style menus/loop
+ */
+
+
+ // Typically, the player will always start within this menu/state when not on an expedition.
+
+
+
+ /*  ========================================================
+        UI Layout / Design
+    ========================================================
+    State - MakerMenu
+    Sub states - Sub-pages of Maker Menu (Adjust training/actions)
+
+    
+*/
+
+
+/*  ========================================================
+        Core Logic and Actions for buttons
+    ========================================================
+    
+*/
+
+// Configuration Sub-menu
+// To make it easy, let's just have the same menu show up twice - checkbox behavior
+// List of actions, and some actions have a sub-menu to optionally refine it further?
+
+
+// Confirmation button - Takes current state of checkboxes and passes onto next state (ExecuteDay)
